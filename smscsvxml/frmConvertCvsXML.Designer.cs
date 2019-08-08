f098fd60f7ef0b6dd1cdbbfffde7563732c52b2c
fe-bo-de-ne <fabio.rossetti@gmail.com>
2019-08-08 22:31:38 +0200

    initial

---
+++ smscsvxml/frmConvertCvsXML.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace smscsvxml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSMS = new System.Windows.Forms.DataGridView();
+            this.ofdScegli = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCSV = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSMS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSMS
+            // 
+            this.dgvSMS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSMS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSMS.Location = new System.Drawing.Point(12, 102);
+            this.dgvSMS.Name = "dgvSMS";
+            this.dgvSMS.Size = new System.Drawing.Size(776, 320);
+            this.dgvSMS.TabIndex = 0;
+            // 
+            // ofdScegli
+            // 
+            this.ofdScegli.FileName = "openFileDialog1";
+            this.ofdScegli.Filter = "File CSV phonecopy|*.csv";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblCSV
+            // 
+            this.lblCSV.AutoSize = true;
+            this.lblCSV.Location = new System.Drawing.Point(162, 62);
+            this.lblCSV.Name = "lblCSV";
+            this.lblCSV.Size = new System.Drawing.Size(49, 13);
+            this.lblCSV.TabIndex = 2;
+            this.lblCSV.Text = "file name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCSV);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvSMS);
+            this.Name = "Form1";
+            this.Text = "CSV to SMS backup XML";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSMS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSMS;
+        private System.Windows.Forms.OpenFileDialog ofdScegli;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCSV;
+    }
+}
+
---
+++ smscsvxml/frmConvertCvsXML.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.IO; 
+using System.Xml.Serialization;
+namespace smscsvxml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (ofdScegli.ShowDialog() != DialogResult.OK)
+            {
+
+            }
+
+            string fname = ofdScegli.FileName;
+
+            string cartella = Path.GetDirectoryName(fname);
+            string file = Path.GetFileName(fname);
+
+            //characterset=65001; is for UTF8
+            lblCSV.Text = file;
+            var filename = fname;
+            var connString = string.Format(
+                @"Provider=Microsoft.Jet.OleDb.4.0; Data Source={0};Extended Properties=""Text;characterset=65001;HDR=YES;FMT=Delimited""",
+                Path.GetDirectoryName(filename)
+            );
+            using (var conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+                var query = "SELECT * FROM [" + Path.GetFileName(filename) + "]";
+                using (var adapter = new OleDbDataAdapter(query, conn))
+                {
+                    var dt = new DataTable("CSV File");
+                    adapter.Fill(dt);
+                    dgvSMS.DataSource = dt;
+
+                    smses smsxml = new smses();
+                    smsxml.count = (ushort)(dt.Rows.Count);
+                    object[] lista = new object[dt.Rows.Count];
+
+                    for (int i =0; i < dt.Rows.Count; i++)
+                    {
+                        smsesSms sms = new smsesSms();
+
+                        //these defaults must be specified
+                        sms.protocol = (byte)(0);
+                        sms.protocolSpecified = true;
+                        sms.toa = "null";
+                        sms.sc_toa = "null";
+                        sms.service_center = "null";
+                        //epoch conversion
+                        sms.address = dt.Rows[i].Field<double?>("Number").ToString();
+                        if (sms.address == "" || sms.address == null) sms.address = "(Unknown)";
+                         sms.date = (ulong)(1000 * (dt.Rows[i].Field<System.DateTime>("Created").ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds);
+                        //net 4.6
+                       // var dateTimeOffset = new DateTimeOffset(dt.Rows[i].Field<System.DateTime>("Created"));
+                       // var unixDateTime = dateTimeOffset.ToUnixTimeSeconds();
+                       // sms.date = (ulong)(unixDateTime * 1000);
+                        switch (dt.Rows[i].Field<String>("Folder"))
+                        {
+                            case "INBOX":
+                                    sms.type = 1;
+                                break;
+
+                            case "OUTBOX":
+                                sms.type = 2;
+                                break;
+
+                            case "DRAFT":
+                                sms.type = 3;
+                                break;
+                            default:
+                                sms.type = 1;
+                                break;
+                        }
+                        sms.subject = null;
+                        sms.body = dt.Rows[i].Field<String>("Text");
+                        sms.contact_name = dt.Rows[i].Field<String>("Sender Name");
+
+                        if (sms.contact_name == "" || sms.contact_name == null) sms.contact_name = "(Unknown)";
+                        sms.status = 0;
+                        sms.read = 1;
+                       // sms.readable_date = dt.Rows[i].Field<System.DateTime>("Created").ToString();
+                        lista[i] = sms;
+                    }
+                    smsxml.Items = lista;
+                    var x = new XmlSerializer(typeof(smses));
+                   string pathbackup = @".\sms-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xml";
+
+                    var fs = new FileStream((pathbackup), FileMode.OpenOrCreate);
+                    x.Serialize(fs, smsxml);
+                    fs.Close();
+                    MessageBox.Show(pathbackup + " created!");
+                }
+            }
+        }
+    }
+}
